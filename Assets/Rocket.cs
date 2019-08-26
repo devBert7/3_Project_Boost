0229b2b3692bb03306f86a38e2ae4c388fb40444
Robert Thiel <thielbert7@gmail.com>
2019-08-26 08:47:52 -0600

    Added ability to rotate rocket and added sounds when using the thruster (space bar).  Adjusted settings to identify better when in play mode what changes can be made.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 	Rigidbody rigidbody;
+	AudioSource audioSource;
+
 	// Start is called before the first frame update
 	void Start() {
 		rigidbody = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
 	}
 
 	// Update is called once per frame
@@ -17,11 +20,16 @@ public class Rocket : MonoBehaviour {
 	private void ProcessInput() {
 		if (Input.GetKey(KeyCode.Space)) {
 			rigidbody.AddRelativeForce(Vector3.up);
+			if (!audioSource.isPlaying) {
+				audioSource.Play();
+				}
+		} else {
+			audioSource.Stop();
 		}
 		if (Input.GetKey(KeyCode.A)) {
-			print("Rotate Left");
+			transform.Rotate(Vector3.forward);
 		} else if (Input.GetKey(KeyCode.D)) {
-			print("Rotate Right");
+			transform.Rotate(-Vector3.forward);
 		}
 	}
 }
